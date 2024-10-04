e79e6adc1d9275f353b80e5b1e9da81043ab466e
AlejandroEG128 <alejandro.escobedo@cbtis128.edu.mx>
2024-10-03 19:17:39 -0600

    Add files via upload

---
+++ Boleano_3w.py
@@ -0,0 +1,33 @@
+#print (10>9)
+#print (10 == 9)
+#print (10<9)
+"""
+a = 200
+b = 33 
+
+if a > b:
+ print("a is grater than b")
+else: 
+ print ("a is not grater than b")
+"""
+
+#identacion
+#print(bool("Hello"))
+#print(bool(15))
+"""
+x=""   #Null value 
+y=0    #Integer number that result is a boolean false
+b=1000
+
+print(bool(x))      #me regresa un valor boleano false 
+print(bool(y))
+print(bool(b))
+"""
+
+bool("abc")
+bool(123)
+
+
+print(bool("abc"))
+print(bool(123))
+
